01727901be9d865ec2df2a1674eff8197207ac29
JuanJegit config --global user.name JuanJegit config --global user.name JuanJe <juanj.rodriguez.sanchez@gmail.com>
2022-07-17 12:27:07 +0200

    Segunda prueba de subida de apuntes

--- tema9/ejercicios/ejercicio_4.py
+++ tema9/ejercicios/ejercicio_4.py
@@ -7,3 +7,41 @@
     cwen@iupui.edu 5
     Ingresa un nombre de archivo: mbox.txt
     zqian@umich.edu 195 """
+try:
+    archivo = open('mbox.txt')
+except:
+    print('No se puede abrir el archivo ', archivo)
+    exit()
+
+diccionario_correos = dict()
+mayor = 0
+valor_mayor = 0
+
+for linea in archivo:
+    linea = linea.rstrip()
+    if linea.startswith('From'):
+        if linea.startswith('From:'):
+            continue
+        palabra = linea.split()
+        if palabra[1] not in diccionario_correos:
+            diccionario_correos[palabra[1]] = 1
+        else:
+            diccionario_correos[palabra[1]] += 1
+        for clave in diccionario_correos:
+            if diccionario_correos[clave] > mayor:
+                mayor = diccionario_correos[clave]
+
+# Solución 1
+for valor_mayor in diccionario_correos:
+    if diccionario_correos[valor_mayor] == mayor:
+        print(valor_mayor, diccionario_correos[valor_mayor])
+
+# Solución 2
+"""
+lista = list(diccionario_correos.keys())[list(diccionario_correos.values()).index(mayor)]
+print(lista, mayor) """
+
+# Solución 3
+"""
+mayor = max(diccionario_correos, key = diccionario_correos.get)
+valor_mayor = diccionario_correos.get(mayor, 0) """
