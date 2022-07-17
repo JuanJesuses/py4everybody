e784cbe0b1f18eebe85ea6c85fb6a4aeffc34b80
JuanJegit config --global user.name JuanJegit config --global user.name JuanJe <juanj.rodriguez.sanchez@gmail.com>
2022-07-17 13:07:08 +0200

    Añado ejercicio 5

---
+++ tema9/ejercicios/ejercicio_5.py
@@ -0,0 +1,3 @@
+""" Ejercicio 5: Este programa almacena el nombre del dominio (en vez de la dirección) desde donde fue enviado
+    el mensaje en vez de quién envió el mensaje (es decir, la dirección de correo electrónica completa). Al
+    final del programa, imprime el contenido de tu diccionario. """
